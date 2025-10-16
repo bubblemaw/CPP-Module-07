ce976c6e6885439e52b3036b5d38deba6db20d57
Mawuli-Kplim Asase <masase@c3r3s2.42lausanne.ch>
2025-10-16 15:54:43 +0200

    ex01 current

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -3,32 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maw <maw@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: masase <masase@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 00:55:40 by maw               #+#    #+#             */
-/*   Updated: 2025/10/15 18:10:23 by maw              ###   ########.fr       */
+/*   Updated: 2025/10/16 14:57:17 by masase           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 # define ITER_HPP
+# include <iostream>
 
 template <typename T>
-void _13(T c)
+void _13(T *array, const int length_2)
 {
-	c += 13;
+    std::cout << "in the function pointers" << std::endl;
 }
 
-template <typename T>
-void iter(T *array, const int length, void *f(T c))
-{
-    int i = 0;
-    while (array[i])
-    {
-        
-    }
-
-}
-
+// template <typename T>
+// void iter(T *array, const int length, void (*f)(T *array, const int length_2))
+// {
+//     f(array, length);
+// }
 
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,10 +6,26 @@
 /*   By: masase <masase@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 14:53:27 by masase            #+#    #+#             */
-/*   Updated: 2025/10/14 14:54:09 by masase           ###   ########.fr       */
+/*   Updated: 2025/10/16 14:57:30 by masase           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
+// void _13(int *array, const int length_2)
+// {
+//     std::cout << "in the function pointers" << std::endl;
+// }
 
+int main()
+{
+    
+    int array[5] = {45, 8, 154, 90, 88};
+    char mot[4] = {'c', 'h', 'a', 'r'};
+
+    void (*fc)(int *array, const int length_2) =_13;
+    
+    (*fc)(array, 5);
+    // iter (array, 5, _13())
+    return (0);
+}
